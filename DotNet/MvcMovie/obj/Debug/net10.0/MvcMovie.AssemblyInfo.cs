6450af8701bb01a6e39fa1ad1ca3a123301cc937
Jan Ciszewski <janciszewski36@gmail.com>
2026-05-16 12:56:05 +0200

    Quick fix to Views

--- DotNet/MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ DotNet/MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5350e4a61e2c61d50370151bedb71f9de09fded4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16d81df81a9bd2a68a6d7e2bbe4ec612184a1fa7")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
